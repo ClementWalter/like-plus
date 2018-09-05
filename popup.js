d40cf129ccf2fe39acd6cfcd00dd5ac3b77c1ae1
ClementWalter <clement0walter@gmail.com>
2018-09-05 19:55:52 +0200

    Add moodValue in storage

--- popup.js
+++ popup.js
@@ -5,11 +5,17 @@ function sendMood() {
   chrome.tabs.query({currentWindow: true, active: true}, function (tabs){
     const activeTab = tabs[0];
     moodValue = document.querySelector('input[name="mood"]').value;
+    chrome.storage.sync.set({mood: moodValue}, function() {
+      console.log('MoodValue is set to ' + moodValue);
+    });
     chrome.tabs.sendMessage(activeTab.id, {text: 'mood', moodValue});
   });
 }
 
 document.addEventListener("DOMContentLoaded", function() {
-  document.querySelector('input[name="mood"]').value = moodValue;
-  document.querySelector('input[name="mood"]').onchange = sendMood;
+  chrome.storage.sync.get(['mood'], function(result) {
+    moodValue = result.mood || moodValue;
+    document.querySelector('input[name="mood"]').value = moodValue;
+    document.querySelector('input[name="mood"]').onchange = sendMood;
+  });
 });
